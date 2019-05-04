314a9280976fa086d49ee441698669c8a776f027
savinngr <savinngr@gmail.com>
2019-05-04 15:27:32 +0530

    Added Tabs inside modal for Login button

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,36 +3,46 @@ import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
 class Header extends Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            modalIsOpen: false
+            modalIsOpen: false,
+            value: 0
         };
     }
 
     openModalHandler = () => {
-        this.setState({modalIsOpen: true})
+        this.setState({ modalIsOpen: true })
     }
 
     closeModalHandler = () => {
-        this.setState({modalIsOpen: false})
+        this.setState({ modalIsOpen: false })
+    }
+
+    tabChangeHandler = (event, value) =>{
+        this.setState({value});
     }
 
     render() {
         return (
             <div>
                 <header className="app-header">
-                <img src={logo} className="app-logo" alt="logo"/>
+                    <img src={logo} className="app-logo" alt="logo" />
                     <div className="login-button">
                         <Button variant="contained" color="default" onClick={this.openModalHandler}> Login </Button>
                     </div>
                 </header>
-                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" 
-                onRequestClose={this.closeModalHandler}>
-                    </Modal>
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+                        <Tab label="LOGIN" />
+                        <Tab label="REGISTER" />
+                    </Tabs>
+                </Modal>
             </div>
         )
     }
